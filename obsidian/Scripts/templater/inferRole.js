68396c2e01df6637225773460a9194aa679baf34
Ivan Roman <DigitalHerencia@Outlook.com>
2026-08-28 14:00:43 -0600

    Return human role names from Templater

--- obsidian/Scripts/templater/inferRole.js
+++ obsidian/Scripts/templater/inferRole.js
@@ -1,15 +1,21 @@
 module.exports = function inferRole(folderPath = "") {
-  const first = String(folderPath).replace(/\\/g, "/").split("/").filter(Boolean)[0] || "";
-  const map = {
-    "Chief of Staff": "chief-of-staff",
-    "Trust Issues": "trust-issues",
-    "Execution": "execution",
-    "Vibes": "vibes",
-    "DevNotes": "devnotes",
-    "Schemes": "schemes",
-    "Prömpter": "prompter",
-    "Prompter": "prompter",
-    "Fuck You Pay Me": "fuck-you-pay-me",
-  };
-  return map[first] || "";
+  const first = String(folderPath)
+    .replace(/\\/g, "/")
+    .split("/")
+    .filter(Boolean)[0] || "";
+
+  if (first === "Prompter") return "Prömpter";
+
+  const roles = new Set([
+    "Chief of Staff",
+    "Trust Issues",
+    "Execution",
+    "Vibes",
+    "DevNotes",
+    "Schemes",
+    "Prömpter",
+    "Fuck You Pay Me",
+  ]);
+
+  return roles.has(first) ? first : "";
 };
